a5e414bf6539eb21fa81e25ad9c6c86d0bd481ba
Gerald Wells <gerald.endeavor@gmail.com>
2019-10-14 10:41:51 -0500

    added command line module for taking arguments

---
+++ lib/commandline/commandline.py
@@ -0,0 +1,20 @@
+import argparse
+
+class CommandLine:
+    arguments = None
+
+    def __init__(self, **kwargs):
+        parser = argparse.ArgumentParser()
+        for k, v in kwargs.items():
+            """
+            Key: -flag
+            Value: help
+            """
+            if isinstance(v, tuple):
+                parser.add_argument(k, action='store_true', help=v)
+            else:
+                parser.add_argument(k, help=v[0])
+        self.arguments = parser.parse_args()
+        print(__name__)
+        print(__package__)
+
